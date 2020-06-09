44b2824201648f3fe942b1b1b9f65e98eae60e4b
Contea <Contea95@naver.com>
2020-06-09 21:15:13 +0900

    자바 조건식 추가

--- Echo_server_pthread/src/Threadserver.java
+++ Echo_server_pthread/src/Threadserver.java
@@ -17,8 +17,6 @@ import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import java.sql.*;
-
 public class Threadserver {
 	private static final int PORT=9190;
 	private static final int THREAD_CNT=5;
@@ -27,10 +25,13 @@ public class Threadserver {
 	private final static String DB_URL = "jdbc:mysql://127.0.0.1:3306/storereservation?serverTimezone=Asia/Seoul";
 	private final static String USER_NAME = "root";
 	private final static String PASSWORD = "12345678";
+	static String full1;
 	
 	static ServerSocket serverSocket = null;
 	
 	Scanner sc = new Scanner(System.in);
+	
+	
 	public static void main(String[] args) {
 		Connection conn = null;
 		Statement state = null;
@@ -49,8 +50,12 @@ public class Threadserver {
 				String storeNumber = rs.getString("storeNumber");
 				String delivery = rs.getString("delivery");
 				String location = rs.getString("location");
-				String full1 = indexNo + " " + storeName + " " + storeNumber + " " + delivery + " " + location;
-				System.out.println(full1);
+				if(full1==null) {
+					full1 = indexNo + " " + storeName + " " + storeNumber + " " + delivery + " " + location +"\n";
+				}else {
+				full1 += indexNo + " " + storeName + " " + storeNumber + " " + delivery + " " + location +"\n";
+				}
+				//System.out.println(full1);
 			}
 			
 			rs.close();
@@ -84,7 +89,7 @@ public class Threadserver {
 			System.out.println("[server] connected by client");
 			System.out.println("[server] Connect with " + socketAddress.getHostString() + " " + socket.getPort());
 			try {
-				threadPool.execute(new ConnectionWrap(socket));
+				threadPool.execute(new ConnectionWrap(socket, full1));
 			}catch(Exception e) {
 				e.printStackTrace();
 			}
@@ -97,12 +102,11 @@ public class Threadserver {
 
 class ConnectionWrap implements Runnable{
 	private Socket socket = null;
-	
-	//DBManager db = new DBManager(); //db관리
-	
+	private String full1=null;
 	ServerSocket serverSocket = null;
-	public ConnectionWrap(Socket socket) {
+	public ConnectionWrap(Socket socket, String full) {
 		this.socket=socket;
+		this.full1=full;
 	}
 	
 	@Override
@@ -112,7 +116,7 @@ class ConnectionWrap implements Runnable{
 				InputStream is = socket.getInputStream();
 				InputStreamReader isr = new InputStreamReader(is, "UTF-8");
 				BufferedReader br = new BufferedReader(isr);
-				//가져와서 StreamWriter, PrintWriter로 감싼다
+				//outputStream 가져와서 StreamWriter, PrintWriter로 감싼다
 				OutputStream os = socket.getOutputStream();
 				OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
 				PrintWriter pw = new PrintWriter(osw, true);
@@ -120,15 +124,27 @@ class ConnectionWrap implements Runnable{
 				
 				String buffer = null;
 				buffer = br.readLine();
-				//db.storeList(); 
 				
 				if(buffer == null) {
 					System.out.println("[server] closed by client");
 					break;
 				}
 				System.out.println("[server] recieved : "+buffer);
+				if(buffer.equals("1")) {
+					System.out.println("다음거 ㄱㄱ");
+					pw.println("다음거 ㄲ");
+					buffer=null;
+					while(true) {
+						buffer=br.readLine();
+						System.out.println("[server] recieved : "+buffer);
+						if(buffer.equals("1")) {
+							System.out.println("그다음거 하자");
+							pw.println("그다음거 하자");
+						}
+					}
+				}
 				//pw.println(buffer);
-				pw.println(intro);
+				pw.println(full1);
 			}
 		}catch(IOException e) {
 			e.printStackTrace();
@@ -142,82 +158,3 @@ class ConnectionWrap implements Runnable{
 		}
 	}
 }
-
-class DBManager {
-	private final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-	private final String DB_URL = "jdbc:mysql://localhost/storereservation?&useSSL=false"; //접속할 DB 서버
-	
-	private final String USER_NAME = "storeDB"; //DB에 접속할 사용자 이름을 상수로 정의
-	private final String PASSWORD = "12345678"; //사용자의 비밀번호를 상수로 정의
-	
-	Connection conn = null;
-	Statement state = null;
-	
-	DBManager(){
-		try {
-			Class.forName(JDBC_DRIVER);
-			conn = DriverManager.getConnection(DB_URL, USER_NAME, PASSWORD);
-			state = conn.createStatement();
-		} catch(ClassNotFoundException cnfe) {       
-			cnfe.printStackTrace();
-		}
-		catch(SQLException se) {
-			se.printStackTrace();
-		}
-		finally {
-			if(conn!=null) try {conn.close();} catch(SQLException se) {}
-			if(state!=null) try {state.close();} catch(SQLException se) {}
-					
-		}
-	}
-	void storeList() {
-		String sql; //SQL문을 저장할 String
-		sql = "SELECT * FROM store";
-		ResultSet rs;
-		try {
-			rs = state.executeQuery(sql);//SQL문을 전달하여 실행
-			System.out.println("번호 | 가게이름 | 빈 좌석 수");
-			while(rs.next()){
-				int indexNo = rs.getInt("indexNo");
-				String storeName = rs.getString("storeName");
-				int emptyTable = rs.getInt("emptyTable");
-				
-				
-				System.out.println(indexNo+ " | "+storeName+" | "+emptyTable); 
-			    }
-			    //rs.close();
-				//state.close();
-				//conn.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} 
-		
-	}
-	void menuList(int indexNo) {
-		String sql; //SQL문을 저장할 String
-		sql = "SELECT menuid, menuName, price FROM menu where indexNo="+indexNo;
-		ResultSet rs;
-		try {
-			rs = state.executeQuery(sql);//SQL문을 전달하여 실행
-			while(rs.next()){
-				int menuid = rs.getInt("indexNo");
-				String storename = rs.getString("storeName");
-				String storeNumber = rs.getString("storeNumber");
-				//String delivery = rs.getString("delivery");
-				int emplyTable = rs.getInt("emptyTable");
-				int watingNumber = rs.getInt("watingNumber");
-				String location = rs.getString("location");
-				//System.out.println("Number: "+ number + "\nName: " + name + "\nKOR: " + kor); 
-				//System.out.println("MATH: "+ math + "\nENG: " + eng + "\n-------------\n");
-		    }
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
-					
-		    
-	}
-			
-		 
-		
-	}
