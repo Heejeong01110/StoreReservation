45e9b59291f4b6450d1de3ef9d69e8b149e4c9c6
Contea <Contea95@naver.com>
2020-06-09 17:36:19 +0900

    자바 스레드풀

---
+++ Echo_server_pthread/src/SimpleThreadPool.java
@@ -0,0 +1,56 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SimpleThreadPool {
+	private static final int PORT = 8080;
+	private static final int THREAD_CNT=5;
+	private static ExecutorService threadPool = Executors.newFixedThreadPool(THREAD_CNT);
+	public static void main(String[] args) {
+		
+		try {
+			ServerSocket serverSocket = new ServerSocket(PORT);
+			
+			while(true) {
+				Socket socket = serverSocket.accept();
+				try {
+					threadPool.execute(new ConnectionWrap1(socket));
+					
+				}catch(Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
+
+
+class ConnectionWrap1 implements Runnable{
+	private Socket socket = null;
+	public ConnectionWrap1(Socket socket) {
+		this.socket=socket;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			OutputStream stream = socket.getOutputStream();
+			stream.write(new Date().toString().getBytes());
+		}catch(IOException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				socket.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
