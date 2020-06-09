2e5ad5173b1eb9eb5dd2a998fe5bb1ec89007d1c
Heejeong01110 <123rkdrkd@naver.com>
2020-06-09 19:31:02 +0900

    db 수정중

--- Echo_server_pthread/src/Threadserver.java
+++ Echo_server_pthread/src/Threadserver.java
@@ -12,11 +12,13 @@ import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import java.sql.*;
+
 public class Threadserver {
 	private static final int PORT=9190;
 	private static final int THREAD_CNT=5;
 	private static ExecutorService threadPool = Executors.newFixedThreadPool(THREAD_CNT);
-
+	
 	static ServerSocket serverSocket = null;
 	
 	Scanner sc = new Scanner(System.in);
@@ -46,6 +48,8 @@ public class Threadserver {
 class ConnectionWrap implements Runnable{
 	private Socket socket = null;
 	
+	//DBManager db = new DBManager(); //db관리
+	
 	ServerSocket serverSocket = null;
 	public ConnectionWrap(Socket socket) {
 		this.socket=socket;
@@ -58,13 +62,14 @@ class ConnectionWrap implements Runnable{
 				InputStream is = socket.getInputStream();
 				InputStreamReader isr = new InputStreamReader(is, "UTF-8");
 				BufferedReader br = new BufferedReader(isr);
-				//outputStream 가져와서 StreamWriter, PrintWriter로 감싼다
+				//가져와서 StreamWriter, PrintWriter로 감싼다
 				OutputStream os = socket.getOutputStream();
 				OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
 				PrintWriter pw = new PrintWriter(osw, true);
 				
 				String buffer = null;
 				buffer = br.readLine();
+				//db.storeList(); 
 				
 				if(buffer == null) {
 					System.out.println("[server] closed by client");
@@ -85,3 +90,82 @@ class ConnectionWrap implements Runnable{
 		}
 	}
 }
+
+class DBManager {
+	private final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	private final String DB_URL = "jdbc:mysql://localhost/storereservation?&useSSL=false"; //접속할 DB 서버
+	
+	private final String USER_NAME = "storeDB"; //DB에 접속할 사용자 이름을 상수로 정의
+	private final String PASSWORD = "12345678"; //사용자의 비밀번호를 상수로 정의
+	
+	Connection conn = null;
+	Statement state = null;
+	
+	DBManager(){
+		try {
+			Class.forName(JDBC_DRIVER);
+			conn = DriverManager.getConnection(DB_URL, USER_NAME, PASSWORD);
+			state = conn.createStatement();
+		} catch(ClassNotFoundException cnfe) {       
+			cnfe.printStackTrace();
+		}
+		catch(SQLException se) {
+			se.printStackTrace();
+		}
+		finally {
+			if(conn!=null) try {conn.close();} catch(SQLException se) {}
+			if(state!=null) try {state.close();} catch(SQLException se) {}
+					
+		}
+	}
+	void storeList() {
+		String sql; //SQL문을 저장할 String
+		sql = "SELECT * FROM store";
+		ResultSet rs;
+		try {
+			rs = state.executeQuery(sql);//SQL문을 전달하여 실행
+			System.out.println("번호 | 가게이름 | 빈 좌석 수");
+			while(rs.next()){
+				int indexNo = rs.getInt("indexNo");
+				String storeName = rs.getString("storeName");
+				int emptyTable = rs.getInt("emptyTable");
+				
+				
+				System.out.println(indexNo+ " | "+storeName+" | "+emptyTable); 
+			    }
+			    //rs.close();
+				//state.close();
+				//conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} 
+		
+	}
+	void menuList(int indexNo) {
+		String sql; //SQL문을 저장할 String
+		sql = "SELECT menuid, menuName, price FROM menu where indexNo="+indexNo;
+		ResultSet rs;
+		try {
+			rs = state.executeQuery(sql);//SQL문을 전달하여 실행
+			while(rs.next()){
+				int menuid = rs.getInt("indexNo");
+				String storename = rs.getString("storeName");
+				String storeNumber = rs.getString("storeNumber");
+				//String delivery = rs.getString("delivery");
+				int emplyTable = rs.getInt("emptyTable");
+				int watingNumber = rs.getInt("watingNumber");
+				String location = rs.getString("location");
+				//System.out.println("Number: "+ number + "\nName: " + name + "\nKOR: " + kor); 
+				//System.out.println("MATH: "+ math + "\nENG: " + eng + "\n-------------\n");
+		    }
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+					
+		    
+	}
+			
+		 
+		
+	}
